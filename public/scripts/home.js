70597633d16f751e29f5ea2e779018ecc5b0530f
Dan Hisey <dchisey@gmail.com>
2017-10-09 15:22:19 -0500

    Changed form to change to confirmation message rather than an alert

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -2,134 +2,6 @@
           ANIMATIONS
 -------------------------------*/
 
-//*******DARKEN PICTURE NEARER TO BUTTON*******
-
-var heroMarketing = document.querySelector('#marketing'),
-    heroCoding = document.querySelector('#coding'),
-    heroImgs = document.querySelectorAll('main>section'),
-    nav = document.querySelector('nav'),
-    h1 = document.querySelector('main>section>h1');
-
-
-function getDistance(x1, x2, y1, y2) {
-  var xs = x2 - x1,
-      ys = y2 - y1;
-
-  xs *= xs;
-  ys *= ys;
-
-  return Math.sqrt(xs + ys);
-}
-
-//add the event listeners to this
-heroImgs.forEach(element => {
-  element.onmouseenter = changeOpacity;
-
-  element.onmouseleave = function(e) {
-    if(e.target.id == 'marketingSection') {
-      heroMarketing.style.opacity = 0.25;
-    } else if (e.target.id == 'codingSection') {
-
-      heroCoding.style.opacity = 0.25;
-    }
-  }
-
-})
-
-
-function changeOpacity(event) {
-  event.target.onmousemove = function(e) {
-    var button = event.target.querySelector('button'),
-        buttonX = button.offsetLeft,
-        buttonY = 415,
-        mouseX = e.clientX,
-        mouseY = e.pageY,
-        section = e.target.id == 'marketingSection' || e.target.id == 'codingSection' ? e.target : e.target.parentNode,
-        id = section.id,
-        distance = section.id == 'codingSection' ? getDistance(buttonX, mouseX - section.clientWidth, buttonY, mouseY) : getDistance(buttonX, mouseX, buttonY, mouseY),
-        height = section.clientHeight;
-
-    if(id == 'marketingSection') {
-      heroMarketing.style.opacity = ((distance / height - 1) * -1) / 2;
-    } else if (id == 'codingSection'){
-      console.log('updated');
-      heroCoding.style.opacity = ((distance / height - 1) * -1) / 2;
-
-    }
-
-  }
-}
-
-//*******create arrows*******
-
-Window.prototype.addMultipleELs = function(events, callback) {
-  var events = events.split(' ');
-  for(var i = 0; i < events.length; i++) {
-    this.addEventListener(events[i], callback);
-  }
-}
-
-var width, height;
-
-window.addMultipleELs('load resize', function() {
-  height = window.innerHeight;
-  width = window.innerWidth;
-})
-
-
-var lines = [...document.querySelectorAll('div')].filter(element => { return element.dataset.line });
-var sections = [...document.querySelectorAll('.allAlign')].map((el, i) => {
-  var correspondingLines = lines.filter(line => {
-    console.log(typeof +line.dataset.index);
-    console.log(`${+line.dataset.index}|${i}, boolean: ${line.dataset.index === i}`);
-    return +line.dataset.index === i;
-  });
-  console.log(correspondingLines);
-
-  return {
-    element: el,
-    index: i,
-    verticalLine: correspondingLines[0],
-    horizontalLine: correspondingLines[1]
-  }
-});
-
-
-
-HTMLElement.prototype.isInViewport = function() {
-  var rect = this.getBoundingClientRect(),
-      html = document.documentElement;
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
-  );
-}
-
-
-window.addEventListener('scroll', function(e){
-  var visibleSections = sections.filter(el => {
-    return el.element.isInViewport();
-  })
-
-  visibleSections.forEach(el => {
-    if (!visibleSections.length) {
-      console.log('There are no elements 100% in viewport.');
-    }
-
-    if (!el.verticalLine.classList.contains('growVL')) {
-      el.verticalLine.classList.add('growVL');
-
-      window.setTimeout(e => {
-        el.horizontalLine.classList.add('growHL');
-      }, 1250);
-    } 
-  })
-
-})
-
 //*******CAROUSEL*******
 
 const slides = document.querySelectorAll('.slide');
@@ -220,7 +92,9 @@ codingButton.addEventListener('click', e => {alert('Not yet!')});
 
 
 //Form handling
-var form = document.querySelector('form');
+var form = document.querySelector('form'),
+    confirmation = document.querySelector('#confirmation h2'),
+    formSection = document.querySelector('#form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -230,7 +104,8 @@ form.addEventListener('submit', e => {
   request.setRequestHeader('Content-type', 'application/json');
   request.send(formData);
 
-  alert("Thanks for sending! I'll be in contact soon.")
+  formSection.makeTransparent();
+  confirmation.appear();
 
 });
 
@@ -246,4 +121,24 @@ function sortFormData(form) {
   })
 
   return JSON.stringify(parsedForm);
+}
+
+
+//Form confirmation
+
+HTMLElement.prototype.makeTransparent = function() {
+  this.style.opacity = 0;
+  this.style['z-index'] = 0;
+}
+
+HTMLElement.prototype.appear = function() {
+  this.style['z-index'] = 1;
+  this.style['font-size'] = '0px';
+  this.style.opacity = 1;
+
+  if(this.style['font-size'] === '0px') {
+    console.log('Animation start.');
+  }
+  
+  this.classList.add('submitted');
 }
